59b2d2c228a4d91c11287179179aac717e321e64
ed elliott <goeddieuk@gmail.com>
2016-07-27 21:45:36 +0100

    adding logging to smoke test

--- src/DacFxed.Tests/SmokeTest.ps1
+++ src/DacFxed.Tests/SmokeTest.ps1
@@ -1,13 +1,24 @@
 ﻿param([string]$newVersion)
 
 $root = [System.IO.Path]::GetDirectoryName($myInvocation.MyCommand.Definition)
-Write-Host $root
+Write-Host "root: $($root)"
+
 Start-Process -Wait -FilePath powershell.exe "$($root)\Deploy\Setup.ps1 TedBert"
+
+Write-Host "started tedbert...."
 
 Start-Process -Wait -FilePath powershell.exe "$($root)\..\DacFxed\DacFxed\UpdateModuleVersion.ps1 1.9.$($newVersion) $($root)\..\DacFxed\DacFxed\bin\Release $($root)\..\DacFxed\DacFxed\bin\Deploy\DacFxed\1.9.$($newVersion)"
 
-cp -Path "$($root)\..\DacFxed\DacFxed\bin\Deploy\DacFxed\1.9.$($newVersion)" -Destination "$($env:USERPROFILE)\Documents\WindowsPowershell\Modules\DacFxed"
+Write-Host "done the version stuff"
+
+cp -Path "$($root)\..\DacFxed\DacFxed\bin\Deploy\DacFxed\1.9.$($newVersion)" -Destination "$($env:USERPROFILE)\Documents\WindowsPowershell\Modules\DacFxed" -Verbose
+
+Write-Host "done copying..."
 
 Get-Module -ListAvailable
 
-Start-Process -Wait -FilePath powershell.exe "$($root)\Deploy\Deploy.ps1 $($root)\..\TestDacPac\bin\Release\TestDacPac.dacpac abc"
+Start-Process -Wait -FilePath powershell.exe "$($root)\Deploy\Deploy.ps1 $($root)\..\TestDacPac\bin\Release\TestDacPac.dacpac abc -verbose"
+
+
+
+Write-Host "all done???"
