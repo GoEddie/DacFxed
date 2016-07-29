3e50aee60bbcb8e52cc75c56bfa339747d8dd6d4
ed elliott <goeddieuk@gmail.com>
2016-07-29 20:39:53 +0100

    smoke test

--- src/DacFxed.Tests/SmokeTest.ps1
+++ src/DacFxed.Tests/SmokeTest.ps1
@@ -59,5 +59,5 @@ Do-Deploy "$($root)\..\TestDacPac\bin\Release\TestDacPac.dacpac" "$($root)\..\Te
 Write-Host "all done??? $($LASTEXITCODE)"
 
 Deploy-PackagePublish
-
+Import-Module PackageManagement
 Publish-Module
