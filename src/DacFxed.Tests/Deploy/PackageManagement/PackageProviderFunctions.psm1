c422d9479cda87e23f250e36a60c5509970aed3b
ed elliott <goeddieuk@gmail.com>
2016-07-29 20:01:26 +0100

    adding packagemanagement to deploy manually

---
+++ src/DacFxed.Tests/Deploy/PackageManagement/PackageProviderFunctions.psm1
@@ -0,0 +1,418 @@
+﻿###
+# ==++==
+#
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+###
+
+<#
+	Overrides the default Write-Debug so that the output gets routed back thru the
+	$request.Debug() function
+#>
+function Write-Debug {
+	param(
+	[Parameter(Mandatory=$true)][string] $message,
+	[parameter(ValueFromRemainingArguments=$true)]
+	[object[]]
+	 $args= @()
+	)
+
+	if( -not $request  ) {
+		if( -not $args  ) {
+			Microsoft.PowerShell.Utility\write-verbose $message
+			return
+		}
+
+		$msg = [system.string]::format($message, $args)
+		Microsoft.PowerShell.Utility\write-verbose $msg
+		return
+	}
+
+	if( -not $args  ) {
+		$null = $request.Debug($message);
+		return
+	}
+	$null = $request.Debug($message,$args);
+}
+
+function Write-Error {
+	param( 
+		[Parameter(Mandatory=$true)][string] $Message,
+		[Parameter()][string] $Category,
+		[Parameter()][string] $ErrorId,
+		[Parameter()][string] $TargetObject
+	)
+
+	$null = $request.Warning($Message);
+}
+
+<#
+	Overrides the default Write-Verbose so that the output gets routed back thru the
+	$request.Verbose() function
+#>
+
+function Write-Verbose{
+	param(
+	[Parameter(Mandatory=$true)][string] $message,
+	[parameter(ValueFromRemainingArguments=$true)]
+	[object[]]
+	 $args= @()
+	)
+
+	if( -not $request ) {
+		if( -not $args ) {
+			Microsoft.PowerShell.Utility\write-verbose $message
+			return
+		}
+
+		$msg = [system.string]::format($message, $args)
+		Microsoft.PowerShell.Utility\write-verbose $msg
+		return
+	}
+
+	if( -not $args ) {
+		$null = $request.Verbose($message);
+		return
+	}
+	$null = $request.Verbose($message,$args);
+}
+
+<#
+	Overrides the default Write-Warning so that the output gets routed back thru the
+	$request.Warning() function
+#>
+
+function Write-Warning{
+	param(
+	[Parameter(Mandatory=$true)][string] $message,
+	[parameter(ValueFromRemainingArguments=$true)]
+	[object[]]
+	 $args= @()
+	)
+
+	if( -not $request ) {
+		if( -not $args ) {
+			Microsoft.PowerShell.Utility\write-warning $message
+			return
+		}
+
+		$msg = [system.string]::format($message, $args)
+		Microsoft.PowerShell.Utility\write-warning $msg
+		return
+	}
+
+	if( -not $args ) {
+		$null = $request.Warning($message);
+		return
+	}
+	$null = $request.Warning($message,$args);
+}
+
+<#
+	Creates a new instance of a PackageSource object
+#>
+function New-PackageSource {
+	param(
+		[Parameter(Mandatory=$true)][string] $name,
+		[Parameter(Mandatory=$true)][string] $location,
+		[Parameter(Mandatory=$true)][bool] $trusted,
+		[Parameter(Mandatory=$true)][bool] $registered,
+		[bool] $valid = $false,
+		[System.Collections.Hashtable] $details = $null
+	)
+
+	return New-Object -TypeName Microsoft.PackageManagement.MetaProvider.PowerShell.PackageSource -ArgumentList $name,$location,$trusted,$registered,$valid,$details
+}
+
+<#
+	Creates a new instance of a SoftwareIdentity object
+#>
+function New-SoftwareIdentity {
+	param(
+		[Parameter(Mandatory=$true)][string] $fastPackageReference,
+		[Parameter(Mandatory=$true)][string] $name,
+		[Parameter(Mandatory=$true)][string] $version,
+		[Parameter(Mandatory=$true)][string] $versionScheme,
+		[Parameter(Mandatory=$true)][string] $source,
+		[string] $summary,
+		[string] $searchKey = $null,
+		[string] $fullPath = $null,
+		[string] $filename = $null,
+		[System.Collections.Hashtable] $details = $null,
+		[System.Collections.ArrayList] $entities = $null,
+		[System.Collections.ArrayList] $links = $null,
+		[bool] $fromTrustedSource = $false,
+		[System.Collections.ArrayList] $dependencies = $null
+	)
+	return New-Object -TypeName Microsoft.PackageManagement.MetaProvider.PowerShell.SoftwareIdentity -ArgumentList $fastPackageReference, $name, $version,  $versionScheme,  $source,  $summary,  $searchKey, $fullPath, $filename , $details , $entities, $links, $fromTrustedSource, $dependencies
+}
+
+<#
+	Creates a new instance of a DyamicOption object
+#>
+function New-DynamicOption {
+	param(
+		[Parameter(Mandatory=$true)][Microsoft.PackageManagement.MetaProvider.PowerShell.OptionCategory] $category,
+		[Parameter(Mandatory=$true)][string] $name,
+		[Parameter(Mandatory=$true)][Microsoft.PackageManagement.MetaProvider.PowerShell.OptionType] $expectedType,
+		[Parameter(Mandatory=$true)][bool] $isRequired,
+		[System.Collections.ArrayList] $permittedValues = $null
+	)
+
+	if( -not $permittedValues ) {
+		return New-Object -TypeName Microsoft.PackageManagement.MetaProvider.PowerShell.DynamicOption -ArgumentList $category,$name,  $expectedType, $isRequired
+	}
+	return New-Object -TypeName Microsoft.PackageManagement.MetaProvider.PowerShell.DynamicOption -ArgumentList $category,$name,  $expectedType, $isRequired, $permittedValues.ToArray()
+}
+
+<#
+	Creates a new instance of a Feature object
+#>
+function New-Feature {
+	param(
+		[Parameter(Mandatory=$true)][string] $name,
+		[System.Collections.ArrayList] $values = $null
+	)
+
+	if( -not $values ) {
+		return New-Object -TypeName Microsoft.PackageManagement.MetaProvider.PowerShell.Feature -ArgumentList $name
+	}
+	return New-Object -TypeName Microsoft.PackageManagement.MetaProvider.PowerShell.Feature -ArgumentList $name, $values.ToArray()
+}
+
+<#
+	Duplicates the $request object and overrides the client-supplied data with the specified values.
+#>
+function New-Request {
+	param(
+		[System.Collections.Hashtable] $options = $null,
+		[System.Collections.ArrayList] $sources = $null,
+		[PSCredential] $credential = $null
+	)
+
+	return $request.CloneRequest( $options, $sources, $credential )
+}
+
+function New-Entity {
+	param(
+		[Parameter(Mandatory=$true)][string] $name,
+		[Parameter(Mandatory=$true,ParameterSetName="role")][string] $role,
+		[Parameter(Mandatory=$true,ParameterSetName="roles")][System.Collections.ArrayList]$roles,
+        [string] $regId = $null,
+        [string] $thumbprint= $null
+	)
+
+	$o = New-Object -TypeName Microsoft.PackageManagement.MetaProvider.PowerShell.Entity
+	$o.Name = $name
+
+	# support role as a NMTOKENS string or an array of strings
+	if( $role ) {
+		$o.Role = $role
+	} 
+	if( $roles )  {
+		$o.Roles = $roles
+	}
+
+	$o.regId = $regId
+	$o.thumbprint = $thumbprint
+	return $o
+}
+
+function New-Link {
+	param(
+		[Parameter(Mandatory=$true)][string] $HRef,
+		[Parameter(Mandatory=$true)][string] $relationship,
+		[string] $mediaType = $null,
+		[string] $ownership = $null,
+		[string] $use= $null,
+		[string] $appliesToMedia= $null,
+		[string] $artifact = $null
+	)
+
+	$o = New-Object -TypeName Microsoft.PackageManagement.MetaProvider.PowerShell.Link
+
+	$o.HRef = $HRef
+	$o.Relationship =$relationship
+	$o.MediaType =$mediaType
+	$o.Ownership =$ownership
+	$o.Use = $use
+	$o.AppliesToMedia = $appliesToMedia
+	$o.Artifact = $artifact
+
+	return $o
+}
+
+function New-Dependency {
+	param(
+		[Parameter(Mandatory=$true)][string] $providerName,
+		[Parameter(Mandatory=$true)][string] $packageName,
+		[string] $version= $null,
+		[string] $source = $null,
+		[string] $appliesTo = $null
+	)
+
+	$o = New-Object -TypeName Microsoft.PackageManagement.MetaProvider.PowerShell.Dependency
+
+	$o.ProviderName = $providerName
+	$o.PackageName =$packageName
+	$o.Version =$version
+	$o.Source =$source
+	$o.AppliesTo = $appliesTo
+
+	return $o
+}
+# SIG # Begin signature block
+# MIIarwYJKoZIhvcNAQcCoIIaoDCCGpwCAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
+# gjcCAQSgWzBZMDQGCisGAQQBgjcCAR4wJgIDAQAABBAfzDtgWUsITrck0sYpfvNR
+# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQUNHzb9FbREux8Q3+USiFQ6M8q
+# RBugghWCMIIEwzCCA6ugAwIBAgITMwAAAJb6gDHvN2RGRQAAAAAAljANBgkqhkiG
+# 9w0BAQUFADB3MQswCQYDVQQGEwJVUzETMBEGA1UECBMKV2FzaGluZ3RvbjEQMA4G
+# A1UEBxMHUmVkbW9uZDEeMBwGA1UEChMVTWljcm9zb2Z0IENvcnBvcmF0aW9uMSEw
+# HwYDVQQDExhNaWNyb3NvZnQgVGltZS1TdGFtcCBQQ0EwHhcNMTUxMDA3MTgxNDI0
+# WhcNMTcwMTA3MTgxNDI0WjCBszELMAkGA1UEBhMCVVMxEzARBgNVBAgTCldhc2hp
+# bmd0b24xEDAOBgNVBAcTB1JlZG1vbmQxHjAcBgNVBAoTFU1pY3Jvc29mdCBDb3Jw
+# b3JhdGlvbjENMAsGA1UECxMETU9QUjEnMCUGA1UECxMebkNpcGhlciBEU0UgRVNO
+# OkJCRUMtMzBDQS0yREJFMSUwIwYDVQQDExxNaWNyb3NvZnQgVGltZS1TdGFtcCBT
+# ZXJ2aWNlMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAm1pYSwjyVGa6
+# tIZe8M6+zXQQ33WKYIyKYcI3oiZcZgVcxdizVjv3hKmjqmRTC5REuLtaSYbdeCuG
+# bdMP2+NGWrqeWKLQIxb/Gs/BkEzrr+ewnZ+UQ7xON8jkhPhMSdT5ZiVVNdhVgo+y
+# 3hvrk0tk4iDpr5Xwqk5U2W5yZkXras/mIIfO54mjfS31tKQbIsxxubm8Np9ioBit
+# boqgiC1iwSxGh7/LGPp1NJVacuQc1JMuzkhRNXxwALbWbyrsUV8Aztz5eaUASLoF
+# jkK43ety0X/rV9Qlws43Q2LjKhztpEaxloEr0gioCAEmkJssDjd1qqCZ6X/bht1e
+# ggluXnz2tQIDAQABo4IBCTCCAQUwHQYDVR0OBBYEFMfD/XvxW9NCtvwEw94qmvuS
+# ht7IMB8GA1UdIwQYMBaAFCM0+NlSRnAK7UD7dvuzK7DDNbMPMFQGA1UdHwRNMEsw
+# SaBHoEWGQ2h0dHA6Ly9jcmwubWljcm9zb2Z0LmNvbS9wa2kvY3JsL3Byb2R1Y3Rz
+# L01pY3Jvc29mdFRpbWVTdGFtcFBDQS5jcmwwWAYIKwYBBQUHAQEETDBKMEgGCCsG
+# AQUFBzAChjxodHRwOi8vd3d3Lm1pY3Jvc29mdC5jb20vcGtpL2NlcnRzL01pY3Jv
+# c29mdFRpbWVTdGFtcFBDQS5jcnQwEwYDVR0lBAwwCgYIKwYBBQUHAwgwDQYJKoZI
+# hvcNAQEFBQADggEBADQzONHGQV0X/NPCsvaZQv26Syn1rUGW85E9wUCgtf0iWG55
+# ntOcHryYkkVIkjB/vd9ixfzGlW2Bz08YdPHJc5he9ZNkfwhjHqW9r6ii06pa4kzE
+# PbgYlLwVRRvxzJwLZpSe56UceM8FmEnsRUSVKzabhLjmiIAFpnNlGgYd6g0eDvxT
+# FM9SOJozV4Mjyb7e+Gv//ZxUeZcTK2S/Nam+B6m/mlRVajUYotCDwziVxrm1irMt
+# a15M55pT3aawt+QrwXaRUMRSRmIgXTHgFWdM3AksQGA0a77rRKGYldX0iPyH2XOw
+# rTHQww9kEcX1r+2R+9QjmsljYc3ZPGnA+2YCADEwggTsMIID1KADAgECAhMzAAAB
+# Cix5rtd5e6asAAEAAAEKMA0GCSqGSIb3DQEBBQUAMHkxCzAJBgNVBAYTAlVTMRMw
+# EQYDVQQIEwpXYXNoaW5ndG9uMRAwDgYDVQQHEwdSZWRtb25kMR4wHAYDVQQKExVN
+# aWNyb3NvZnQgQ29ycG9yYXRpb24xIzAhBgNVBAMTGk1pY3Jvc29mdCBDb2RlIFNp
+# Z25pbmcgUENBMB4XDTE1MDYwNDE3NDI0NVoXDTE2MDkwNDE3NDI0NVowgYMxCzAJ
+# BgNVBAYTAlVTMRMwEQYDVQQIEwpXYXNoaW5ndG9uMRAwDgYDVQQHEwdSZWRtb25k
+# MR4wHAYDVQQKExVNaWNyb3NvZnQgQ29ycG9yYXRpb24xDTALBgNVBAsTBE1PUFIx
+# HjAcBgNVBAMTFU1pY3Jvc29mdCBDb3Jwb3JhdGlvbjCCASIwDQYJKoZIhvcNAQEB
+# BQADggEPADCCAQoCggEBAJL8bza74QO5KNZG0aJhuqVG+2MWPi75R9LH7O3HmbEm
+# UXW92swPBhQRpGwZnsBfTVSJ5E1Q2I3NoWGldxOaHKftDXT3p1Z56Cj3U9KxemPg
+# 9ZSXt+zZR/hsPfMliLO8CsUEp458hUh2HGFGqhnEemKLwcI1qvtYb8VjC5NJMIEb
+# e99/fE+0R21feByvtveWE1LvudFNOeVz3khOPBSqlw05zItR4VzRO/COZ+owYKlN
+# Wp1DvdsjusAP10sQnZxN8FGihKrknKc91qPvChhIqPqxTqWYDku/8BTzAMiwSNZb
+# /jjXiREtBbpDAk8iAJYlrX01boRoqyAYOCj+HKIQsaUCAwEAAaOCAWAwggFcMBMG
+# A1UdJQQMMAoGCCsGAQUFBwMDMB0GA1UdDgQWBBSJ/gox6ibN5m3HkZG5lIyiGGE3
+# NDBRBgNVHREESjBIpEYwRDENMAsGA1UECxMETU9QUjEzMDEGA1UEBRMqMzE1OTUr
+# MDQwNzkzNTAtMTZmYS00YzYwLWI2YmYtOWQyYjFjZDA1OTg0MB8GA1UdIwQYMBaA
+# FMsR6MrStBZYAck3LjMWFrlMmgofMFYGA1UdHwRPME0wS6BJoEeGRWh0dHA6Ly9j
+# cmwubWljcm9zb2Z0LmNvbS9wa2kvY3JsL3Byb2R1Y3RzL01pY0NvZFNpZ1BDQV8w
+# OC0zMS0yMDEwLmNybDBaBggrBgEFBQcBAQROMEwwSgYIKwYBBQUHMAKGPmh0dHA6
+# Ly93d3cubWljcm9zb2Z0LmNvbS9wa2kvY2VydHMvTWljQ29kU2lnUENBXzA4LTMx
+# LTIwMTAuY3J0MA0GCSqGSIb3DQEBBQUAA4IBAQCmqFOR3zsB/mFdBlrrZvAM2PfZ
+# hNMAUQ4Q0aTRFyjnjDM4K9hDxgOLdeszkvSp4mf9AtulHU5DRV0bSePgTxbwfo/w
+# iBHKgq2k+6apX/WXYMh7xL98m2ntH4LB8c2OeEti9dcNHNdTEtaWUu81vRmOoECT
+# oQqlLRacwkZ0COvb9NilSTZUEhFVA7N7FvtH/vto/MBFXOI/Enkzou+Cxd5AGQfu
+# FcUKm1kFQanQl56BngNb/ErjGi4FrFBHL4z6edgeIPgF+ylrGBT6cgS3C6eaZOwR
+# XU9FSY0pGi370LYJU180lOAWxLnqczXoV+/h6xbDGMcGszvPYYTitkSJlKOGMIIF
+# vDCCA6SgAwIBAgIKYTMmGgAAAAAAMTANBgkqhkiG9w0BAQUFADBfMRMwEQYKCZIm
+# iZPyLGQBGRYDY29tMRkwFwYKCZImiZPyLGQBGRYJbWljcm9zb2Z0MS0wKwYDVQQD
+# EyRNaWNyb3NvZnQgUm9vdCBDZXJ0aWZpY2F0ZSBBdXRob3JpdHkwHhcNMTAwODMx
+# MjIxOTMyWhcNMjAwODMxMjIyOTMyWjB5MQswCQYDVQQGEwJVUzETMBEGA1UECBMK
+# V2FzaGluZ3RvbjEQMA4GA1UEBxMHUmVkbW9uZDEeMBwGA1UEChMVTWljcm9zb2Z0
+# IENvcnBvcmF0aW9uMSMwIQYDVQQDExpNaWNyb3NvZnQgQ29kZSBTaWduaW5nIFBD
+# QTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBALJyWVwZMGS/HZpgICBC
+# mXZTbD4b1m/My/Hqa/6XFhDg3zp0gxq3L6Ay7P/ewkJOI9VyANs1VwqJyq4gSfTw
+# aKxNS42lvXlLcZtHB9r9Jd+ddYjPqnNEf9eB2/O98jakyVxF3K+tPeAoaJcap6Vy
+# c1bxF5Tk/TWUcqDWdl8ed0WDhTgW0HNbBbpnUo2lsmkv2hkL/pJ0KeJ2L1TdFDBZ
+# +NKNYv3LyV9GMVC5JxPkQDDPcikQKCLHN049oDI9kM2hOAaFXE5WgigqBTK3S9dP
+# Y+fSLWLxRT3nrAgA9kahntFbjCZT6HqqSvJGzzc8OJ60d1ylF56NyxGPVjzBrAlf
+# A9MCAwEAAaOCAV4wggFaMA8GA1UdEwEB/wQFMAMBAf8wHQYDVR0OBBYEFMsR6MrS
+# tBZYAck3LjMWFrlMmgofMAsGA1UdDwQEAwIBhjASBgkrBgEEAYI3FQEEBQIDAQAB
+# MCMGCSsGAQQBgjcVAgQWBBT90TFO0yaKleGYYDuoMW+mPLzYLTAZBgkrBgEEAYI3
+# FAIEDB4KAFMAdQBiAEMAQTAfBgNVHSMEGDAWgBQOrIJgQFYnl+UlE/wq4QpTlVnk
+# pDBQBgNVHR8ESTBHMEWgQ6BBhj9odHRwOi8vY3JsLm1pY3Jvc29mdC5jb20vcGtp
+# L2NybC9wcm9kdWN0cy9taWNyb3NvZnRyb290Y2VydC5jcmwwVAYIKwYBBQUHAQEE
+# SDBGMEQGCCsGAQUFBzAChjhodHRwOi8vd3d3Lm1pY3Jvc29mdC5jb20vcGtpL2Nl
+# cnRzL01pY3Jvc29mdFJvb3RDZXJ0LmNydDANBgkqhkiG9w0BAQUFAAOCAgEAWTk+
+# fyZGr+tvQLEytWrrDi9uqEn361917Uw7LddDrQv+y+ktMaMjzHxQmIAhXaw9L0y6
+# oqhWnONwu7i0+Hm1SXL3PupBf8rhDBdpy6WcIC36C1DEVs0t40rSvHDnqA2iA6VW
+# 4LiKS1fylUKc8fPv7uOGHzQ8uFaa8FMjhSqkghyT4pQHHfLiTviMocroE6WRTsgb
+# 0o9ylSpxbZsa+BzwU9ZnzCL/XB3Nooy9J7J5Y1ZEolHN+emjWFbdmwJFRC9f9Nqu
+# 1IIybvyklRPk62nnqaIsvsgrEA5ljpnb9aL6EiYJZTiU8XofSrvR4Vbo0HiWGFzJ
+# NRZf3ZMdSY4tvq00RBzuEBUaAF3dNVshzpjHCe6FDoxPbQ4TTj18KUicctHzbMrB
+# 7HCjV5JXfZSNoBtIA1r3z6NnCnSlNu0tLxfI5nI3EvRvsTxngvlSso0zFmUeDord
+# EN5k9G/ORtTTF+l5xAS00/ss3x+KnqwK+xMnQK3k+eGpf0a7B2BHZWBATrBC7E7t
+# s3Z52Ao0CW0cgDEf4g5U3eWh++VHEK1kmP9QFi58vwUheuKVQSdpw5OPlcmN2Jsh
+# rg1cnPCiroZogwxqLbt2awAdlq3yFnv2FoMkuYjPaqhHMS+a3ONxPdcAfmJH0c6I
+# ybgY+g5yjcGjPa8CQGr/aZuW4hCoELQ3UAjWwz0wggYHMIID76ADAgECAgphFmg0
+# AAAAAAAcMA0GCSqGSIb3DQEBBQUAMF8xEzARBgoJkiaJk/IsZAEZFgNjb20xGTAX
+# BgoJkiaJk/IsZAEZFgltaWNyb3NvZnQxLTArBgNVBAMTJE1pY3Jvc29mdCBSb290
+# IENlcnRpZmljYXRlIEF1dGhvcml0eTAeFw0wNzA0MDMxMjUzMDlaFw0yMTA0MDMx
+# MzAzMDlaMHcxCzAJBgNVBAYTAlVTMRMwEQYDVQQIEwpXYXNoaW5ndG9uMRAwDgYD
+# VQQHEwdSZWRtb25kMR4wHAYDVQQKExVNaWNyb3NvZnQgQ29ycG9yYXRpb24xITAf
+# BgNVBAMTGE1pY3Jvc29mdCBUaW1lLVN0YW1wIFBDQTCCASIwDQYJKoZIhvcNAQEB
+# BQADggEPADCCAQoCggEBAJ+hbLHf20iSKnxrLhnhveLjxZlRI1Ctzt0YTiQP7tGn
+# 0UytdDAgEesH1VSVFUmUG0KSrphcMCbaAGvoe73siQcP9w4EmPCJzB/LMySHnfL0
+# Zxws/HvniB3q506jocEjU8qN+kXPCdBer9CwQgSi+aZsk2fXKNxGU7CG0OUoRi4n
+# rIZPVVIM5AMs+2qQkDBuh/NZMJ36ftaXs+ghl3740hPzCLdTbVK0RZCfSABKR2YR
+# JylmqJfk0waBSqL5hKcRRxQJgp+E7VV4/gGaHVAIhQAQMEbtt94jRrvELVSfrx54
+# QTF3zJvfO4OToWECtR0Nsfz3m7IBziJLVP/5BcPCIAsCAwEAAaOCAaswggGnMA8G
+# A1UdEwEB/wQFMAMBAf8wHQYDVR0OBBYEFCM0+NlSRnAK7UD7dvuzK7DDNbMPMAsG
+# A1UdDwQEAwIBhjAQBgkrBgEEAYI3FQEEAwIBADCBmAYDVR0jBIGQMIGNgBQOrIJg
+# QFYnl+UlE/wq4QpTlVnkpKFjpGEwXzETMBEGCgmSJomT8ixkARkWA2NvbTEZMBcG
+# CgmSJomT8ixkARkWCW1pY3Jvc29mdDEtMCsGA1UEAxMkTWljcm9zb2Z0IFJvb3Qg
+# Q2VydGlmaWNhdGUgQXV0aG9yaXR5ghB5rRahSqClrUxzWPQHEy5lMFAGA1UdHwRJ
+# MEcwRaBDoEGGP2h0dHA6Ly9jcmwubWljcm9zb2Z0LmNvbS9wa2kvY3JsL3Byb2R1
+# Y3RzL21pY3Jvc29mdHJvb3RjZXJ0LmNybDBUBggrBgEFBQcBAQRIMEYwRAYIKwYB
+# BQUHMAKGOGh0dHA6Ly93d3cubWljcm9zb2Z0LmNvbS9wa2kvY2VydHMvTWljcm9z
+# b2Z0Um9vdENlcnQuY3J0MBMGA1UdJQQMMAoGCCsGAQUFBwMIMA0GCSqGSIb3DQEB
+# BQUAA4ICAQAQl4rDXANENt3ptK132855UU0BsS50cVttDBOrzr57j7gu1BKijG1i
+# uFcCy04gE1CZ3XpA4le7r1iaHOEdAYasu3jyi9DsOwHu4r6PCgXIjUji8FMV3U+r
+# kuTnjWrVgMHmlPIGL4UD6ZEqJCJw+/b85HiZLg33B+JwvBhOnY5rCnKVuKE5nGct
+# xVEO6mJcPxaYiyA/4gcaMvnMMUp2MT0rcgvI6nA9/4UKE9/CCmGO8Ne4F+tOi3/F
+# NSteo7/rvH0LQnvUU3Ih7jDKu3hlXFsBFwoUDtLaFJj1PLlmWLMtL+f5hYbMUVbo
+# nXCUbKw5TNT2eb+qGHpiKe+imyk0BncaYsk9Hm0fgvALxyy7z0Oz5fnsfbXjpKh0
+# NbhOxXEjEiZ2CzxSjHFaRkMUvLOzsE1nyJ9C/4B5IYCeFTBm6EISXhrIniIh0EPp
+# K+m79EjMLNTYMoBMJipIJF9a6lbvpt6Znco6b72BJ3QGEe52Ib+bgsEnVLaxaj2J
+# oXZhtG6hE6a/qkfwEm/9ijJssv7fUciMI8lmvZ0dhxJkAj0tr1mPuOQh5bWwymO0
+# eFQF1EEuUKyUsKV4q7OglnUa2ZKHE3UiLzKoCG6gW4wlv6DvhMoh1useT8ma7kng
+# 9wFlb4kLfchpyOZu6qeXzjEp/w7FW1zYTRuh2Povnj8uVRZryROj/TGCBJcwggST
+# AgEBMIGQMHkxCzAJBgNVBAYTAlVTMRMwEQYDVQQIEwpXYXNoaW5ndG9uMRAwDgYD
+# VQQHEwdSZWRtb25kMR4wHAYDVQQKExVNaWNyb3NvZnQgQ29ycG9yYXRpb24xIzAh
+# BgNVBAMTGk1pY3Jvc29mdCBDb2RlIFNpZ25pbmcgUENBAhMzAAABCix5rtd5e6as
+# AAEAAAEKMAkGBSsOAwIaBQCggbAwGQYJKoZIhvcNAQkDMQwGCisGAQQBgjcCAQQw
+# HAYKKwYBBAGCNwIBCzEOMAwGCisGAQQBgjcCARUwIwYJKoZIhvcNAQkEMRYEFPyc
+# RHKy2MXxXf5LJSGQWjmoYBlQMFAGCisGAQQBgjcCAQwxQjBAoBaAFABQAG8AdwBl
+# AHIAUwBoAGUAbABsoSaAJGh0dHA6Ly93d3cubWljcm9zb2Z0LmNvbS9Qb3dlclNo
+# ZWxsIDANBgkqhkiG9w0BAQEFAASCAQAhZ+FOOb0o09dz7NazSYvwnCfrsMWYQ17B
+# K+b0stJdxcPxxQTJJQh5SkkLThJFSaxXaoUbiz7+JXk5poVIqkMi0ZUEN6j5P8Az
+# Agj4ORxU5G//RVgAbZ3xfmCtRBzduaE288fZFcKQsYG2MUeQXseIwv193iJ4qNq7
+# gNvYj29qsMF2IrTlJMMCNgv1hxq5kvdSRTaNHJzkY2CJOj6NWpg01klwgmUrsAVV
+# A5KJGNghINU6XfIfRt4knhcr+RKPLyuiHKizFyNhhN5NVyfUEPkpD+i5NCyPufAa
+# ekZKQRDsAac8xlJh+IpA51ItOITPU7O4Wg85rPgRveTrDLqbdAijoYICKDCCAiQG
+# CSqGSIb3DQEJBjGCAhUwggIRAgEBMIGOMHcxCzAJBgNVBAYTAlVTMRMwEQYDVQQI
+# EwpXYXNoaW5ndG9uMRAwDgYDVQQHEwdSZWRtb25kMR4wHAYDVQQKExVNaWNyb3Nv
+# ZnQgQ29ycG9yYXRpb24xITAfBgNVBAMTGE1pY3Jvc29mdCBUaW1lLVN0YW1wIFBD
+# QQITMwAAAJb6gDHvN2RGRQAAAAAAljAJBgUrDgMCGgUAoF0wGAYJKoZIhvcNAQkD
+# MQsGCSqGSIb3DQEHATAcBgkqhkiG9w0BCQUxDxcNMTYwMjI5MjAwMTMyWjAjBgkq
+# hkiG9w0BCQQxFgQU0nH7S21b9in2R2/Qp+oWcFC0YycwDQYJKoZIhvcNAQEFBQAE
+# ggEAP+llGcnpl8rAq1EDK3mlBbs1D9Dn9w2rcY//AXguAkHwcGyfyqIa8Kg5Bwyr
+# RLaBsuIpCSHhwe0bog3AMrYDIRtqv7ojudTIQ40Y0ywMj2lAyYEkQkWpgqrA06Le
+# 9bOVf14tveWX/ClEJzDujugPy3GKwy+Kw0ZaB+DA9OtiJmjKzwsXjYQ7LhEt7m1R
+# 2mWml9WFdYbs54+0HWFp+E2i/nyA6E21dXAGZQDVpCyMLkO7hLeUYbuxUmQu16tj
+# 2hz+V2XDu7KAlkZyrsiG8eijUe/T3LPxoIb/YZs2xaD+oXvgBuutfV5Xgx4DH8ap
+# HF0uIZWPtYVAxRhHAW0XcKebKQ==
+# SIG # End signature block
--- src/DacFxed.Tests/SmokeTest.ps1
+++ src/DacFxed.Tests/SmokeTest.ps1
@@ -30,25 +30,23 @@ Publish-Database -DacpacPath $dacpacPath -PublishProfilePath $publicProfilePath 
 
 }
 
+
+function Deploy-PackagePublish{
+
+    cp -Path "$($root)\..\DacFxed.Tests\Deploy\PackageManagement" -Destination  "$($env:USERPROFILE)\Documents\WindowsPowershell\Modules\PackgeManagement" -Verbose -Force -Recurse
+
+}
+
 $root = [System.IO.Path]::GetDirectoryName($myInvocation.MyCommand.Definition)
+
 Write-Host "root: $($root)"
 Setup-LocalDb "TedBert"
-#Start-ProcessWithLogging "powershell.exe" "-ExecutionPolicy RemoteSigned -File $($root)\Deploy\Setup.ps1 TedBert"
 
 Write-Host "started tedbert....$($LASTEXITCODE)"
-
-#Start-Process -Wait -FilePath powershell.exe "$($root)\..\DacFxed\DacFxed\UpdateModuleVersion.ps1 1.9.$($newVersion) $($sourceDir) $($root)\..\DacFxed\DacFxed\bin\Deploy\DacFxed\1.9.$($newVersion)"
 
 Write-Host "done the version stuff $($LASTEXITCODE)"
 
 cp -Path "$($sourceDir)" -Destination "$($env:USERPROFILE)\Documents\WindowsPowershell\Modules\DacFxed" -Verbose -Force -Recurse
-
-ls "$($env:USERPROFILE)\Documents\WindowsPowershell\Modules\DacFxed"
-Write-Host "ls 1"
-ls  "$($env:USERPROFILE)\Documents\WindowsPowershell\Modules"
-Write-Host "ls 2"
-ls C:\Users\buildguest\Documents\WindowsPowerShell\Modules -Recurse
-Write-Host "ls 3"
 
 
 Write-Host "done copying...$($LASTEXITCODE)"
@@ -58,10 +56,7 @@ Get-Module DacFxed
 #Start-ProcessWithLogging  "powershell.exe" "-ExecutionPolicy RemoteSigned -File $($root)\Deploy\Deploy.ps1 $($root)\..\TestDacPac\bin\Release\TestDacPac.dacpac abc -verbose"
 Do-Deploy "$($root)\..\TestDacPac\bin\Release\TestDacPac.dacpac" "$($root)\..\TestDacPac\bin\Release\TestDacPac.publish.xml" "www"
 
-
-ls C:\Users\buildguest\Documents\WindowsPowerShell\Modules -Recurse
-Write-Host "ls 4"
-
-
 Write-Host "all done??? $($LASTEXITCODE)"
 
+
+Publish-Module
